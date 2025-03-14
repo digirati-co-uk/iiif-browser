e56fa2b9202fea51a37284525aba8e946a5168b2
Stephen Fraser <stephen.fraser@digirati.com>
2025-03-14 15:41:18 +0000

    split styles, made bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig, type Options } from "tsup";
 import cssPlugin from "esbuild-plugin-react18-css";
+import { type Options, defineConfig } from "tsup";
 
 export default defineConfig((ctx) => {
   return {
     dts: true,
+
+    entry: {
+      index: "./src/v2/bundle.ts",
+      v1: "./src/index.ts",
+    },
+
     target: ["es2020"],
     format: ["esm", "cjs", "iife"],
     esbuildPlugins: [cssPlugin()],
     globalName: "IIIFBrowser",
     platform: "browser",
-    minify: true,
+    minify: false,
     external: [
       // -
       "@iiif/parser",
